902b7daabeca82774bdebea7ed7cf3474b24b93b
Armaghan Mahmood Shams <arma123ghan@gmail.com>
2026-08-07 23:57:37 +0500

    made all test functions static

--- tests/test_complex_num.c
+++ tests/test_complex_num.c
@@ -5,21 +5,21 @@
 #include <complex/complex_constants.h>
 #include <complex/complex_utils.h>
 
-void test_creation(int*, int*);
-void test_equality(int*, int*);
-void test_parsing(int*, int*);
-void test_pure_real(int* p, int* f);
-void test_pure_imaginary(int* p, int* f);
-void test_addition(int* p, int* f);
-void test_subtraction(int* p, int* f);
-void test_scalar_multiplication(int* p, int* f);
-void test_complex_multiplication(int* p, int* f);
-void test_conjugate(int* p, int* f);
-void test_division(int* p, int* f);
-void test_reciprocal(int* p, int* f);
-void test_display(int* p, int* f);
-void display_create_complex(double,double,int*,int*);
-void test_display(int*,int*);
+static void test_creation(int*, int*);
+static void test_equality(int*, int*);
+static void test_parsing(int*, int*);
+static void test_pure_real(int* , int* );
+static void test_pure_imaginary(int* , int* );
+static void test_addition(int* , int* );
+static void test_subtraction(int* , int* );
+static void test_scalar_multiplication(int* , int* );
+static void test_complex_multiplication(int* , int* );
+static void test_conjugate(int* , int* );
+static void test_division(int* , int* );
+static void test_reciprocal(int* , int* );
+static void test_display(int* , int* );
+static void display_create_complex(double,double,int*,int*);
+static void test_display(int*,int*);
 
 void test_complex_num(int* total_pass, int* total_fail)
 {
@@ -46,7 +46,7 @@ void test_complex_num(int* total_pass, int* total_fail)
     *total_fail += fail;
 }
 
-void test_creation(int *p, int* f)
+static void test_creation(int *p, int* f)
 {
     cvt_complex z = create_complex(3.0, 4.0);
 
@@ -55,7 +55,7 @@ void test_creation(int *p, int* f)
 
 }
 
-void test_equality(int* p, int* f)
+static void test_equality(int* p, int* f)
 {
     ASSERT(complex_equals(
     create_complex(1,2),
@@ -78,7 +78,7 @@ void test_equality(int* p, int* f)
     create_complex(INFINITY,INFINITY)),p,f);
 }
 
-void test_parsing(int* p, int* f) 
+static void test_parsing(int* p, int* f) 
 {
     ASSERT(complex_equals(
         create_from_string("5"),
@@ -121,7 +121,7 @@ void test_parsing(int* p, int* f)
         COMPLEX_ZERO), p, f, 1);
 }
 
-void test_modulus(int* p, int* f) 
+static void test_modulus(int* p, int* f) 
 {
     ASSERT(is_nearly_equal(cvt_modulus(create_complex(3, 4)),5.0), p, f);
 
@@ -132,7 +132,7 @@ void test_modulus(int* p, int* f)
     ASSERT_F(cvt_modulus(create_complex(1e308, 1e308)) == 0.0, p, f, 1);
 }
 
-void test_argument(int* p, int* f) 
+static void test_argument(int* p, int* f) 
 {
     ASSERT(is_nearly_equal(
         cvt_argument(create_complex(1, 0)), 
@@ -158,21 +158,21 @@ void test_argument(int* p, int* f)
         cvt_argument(create_complex(1e-15, 1e-15)), 
         PI_BY_FOUR), p, f, 1);
 }
-void test_pure_real(int* p, int* f) 
+static void test_pure_real(int* p, int* f) 
 {
     ASSERT(is_purely_real(create_complex(5, 0)), p, f);
     ASSERT(!is_purely_real(create_complex(5, 2)), p, f);
     ASSERT_F(is_purely_real(create_complex(5.0000001, 0)), p, f, 1);
 }
 
-void test_pure_imaginary(int* p, int* f) 
+static void test_pure_imaginary(int* p, int* f) 
 {
     ASSERT(is_purely_imaginary(create_complex(0, 5)), p, f);
     ASSERT(!is_purely_imaginary(create_complex(1, 5)), p, f);
     ASSERT_F(is_purely_imaginary(create_complex(0, 5.0000001)), p, f, 1);
 }
 
-void test_addition(int* p, int* f) 
+static void test_addition(int* p, int* f) 
 {
     ASSERT(complex_equals(
         cvt_add(
@@ -187,7 +187,7 @@ void test_addition(int* p, int* f)
         COMPLEX_ZERO), p, f, 1);
 }
 
-void test_subtraction(int* p, int* f) 
+static void test_subtraction(int* p, int* f) 
 {
     ASSERT(complex_equals(
         cvt_subtract(
@@ -202,7 +202,7 @@ void test_subtraction(int* p, int* f)
         create_complex(1e-15, 0)), p, f, 1);
 }
 
-void test_scalar_multiplication(int* p, int* f) 
+static void test_scalar_multiplication(int* p, int* f) 
 {
     ASSERT(complex_equals(
         cvt_multiply_scalar(
@@ -217,7 +217,7 @@ void test_scalar_multiplication(int* p, int* f)
         COMPLEX_ZERO), p, f, 1);
 }
 
-void test_complex_multiplication(int* p, int* f) 
+static void test_complex_multiplication(int* p, int* f) 
 {
     ASSERT(complex_equals(
         cvt_multiply(
@@ -250,7 +250,7 @@ void test_complex_multiplication(int* p, int* f)
         COMPLEX_ZERO), p, f, 1);
 }
 
-void test_conjugate(int* p, int* f) 
+static void test_conjugate(int* p, int* f) 
 {
     ASSERT(complex_equals(
         cvt_conjugate(
@@ -263,7 +263,7 @@ void test_conjugate(int* p, int* f)
         create_complex(0, 1)), p, f, 1);
 }
 
-void test_division(int* p, int* f) 
+static void test_division(int* p, int* f) 
 {
     ASSERT(complex_equals(
         cvt_divide(
@@ -290,7 +290,7 @@ void test_division(int* p, int* f)
         COMPLEX_ZERO), p, f, 1);
 }
 
-void test_reciprocal(int* p, int* f) 
+static void test_reciprocal(int* p, int* f) 
 {
     ASSERT(complex_equals(
         cvt_reciprocal(
@@ -308,7 +308,7 @@ void test_reciprocal(int* p, int* f)
         COMPLEX_ZERO), p, f, 1);
 }
 
-void display_create_complex(double real, double img,int* p, int* f) 
+static void display_create_complex(double real, double img,int* p, int* f) 
 {
     char ch;
     display_complex(create_complex(real, img));
@@ -323,7 +323,7 @@ void display_create_complex(double real, double img,int* p, int* f)
     
 }
 
-void test_display(int* p, int* f)
+static void test_display(int* p, int* f)
 {
     display_create_complex(3,4,p,f);
     display_create_complex(-1,0,p,f);
